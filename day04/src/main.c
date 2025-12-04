d99aed761b55058acf1fbddec898fe3354f83d38
Ruben Madsen <42476677+rubenmadsen@users.noreply.github.com>
2025-12-04 06:52:44 +0100

    Added day 4

--- day04/src/main.c
+++ day04/src/main.c
@@ -2,18 +2,21 @@
 #include <unistd.h>
 #include "../../stdlib/file.h"
 #include <stdbool.h>
+#include <string.h>
 
 bool example = true;
 int main(){
-    if(chdir("/Users/rubenmadsen/Advent of code/2025/template") != 0){
+    if(chdir("/Users/rubenmadsen/Advent of code/2025/day04") != 0){
         perror("chdir");
         return 1;
     }
     char *data = example ? fileDump("example_input1.txt") : fileDump("main_input1.txt");
     char *token = strtok(data, "\n");
+    
     while (token){
+        printf("%s\n", token);
         token = strtok(NULL, "\n");
     }
-    printf("Data: %s\n", data);
+    printf("%s\n", token);
     return 0;
 }
